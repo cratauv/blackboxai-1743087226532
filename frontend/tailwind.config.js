0bfb769144830408597bb6248ade71a0c7b905c1
cratauv <81857838+cratauv@users.noreply.github.com>
2025-03-27 16:54:21 +0200

    Add frontend/tailwind.config.js 1743087260684

---
+++ frontend/tailwind.config.js
@@ -0,0 +1,30 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{html,ts}",
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ['Inter', 'sans-serif'],
+      },
+      colors: {
+        'primary': {
+          50: '#fff1f1',
+          100: '#ffe1e1',
+          200: '#ffc7c7',
+          300: '#ffa0a0',
+          400: '#ff6b6b',
+          500: '#ff3b3b',
+          600: '#ff1111',
+          700: '#e70000',
+          800: '#c00',
+          900: '#a00',
+        }
+      }
+    },
+  },
+  plugins: [
+    require('@tailwindcss/forms'),
+  ],
+}
