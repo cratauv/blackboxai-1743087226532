53486e4ea5ea069023d33eb57aa67ee1c22ac137
cratauv <81857838+cratauv@users.noreply.github.com>
2025-03-31 18:09:37 +0300

    Add backend/routes/auth.routes.js 1743433777233

--- backend/routes/auth.routes.js
+++ backend/routes/auth.routes.js
@@ -1,20 +1,12 @@
 const express = require('express');
-const {
-    register,
-    login,
-    logout,
-    getMe,
-    updateDetails,
-    updatePassword
-} = require('../controllers/auth.controller');
+const { register, login, logout, getMe, updateDetails, updatePassword } = require('../controllers/auth.controller');
+const { protect } = require('../middleware/auth');
 
 const router = express.Router();
 
-const { protect } = require('../middleware/auth');
-
 router.post('/register', register);
 router.post('/login', login);
-router.get('/logout', logout);
+router.get('/logout', protect, logout);
 router.get('/me', protect, getMe);
 router.put('/updatedetails', protect, updateDetails);
 router.put('/updatepassword', protect, updatePassword);
